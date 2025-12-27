113465d067c860b301015e23cd86ae3b5c7c45ee
Justus Güttler <jguettler@gunter.software>
2025-12-27 19:37:39 +0100

    I forgot the /region select command. Why is this region type stuff cluttered around hundreds of files?

--- worldguard-core/src/main/java/com/sk89q/worldguard/protection/util/WorldEditRegionConverter.java
+++ worldguard-core/src/main/java/com/sk89q/worldguard/protection/util/WorldEditRegionConverter.java
@@ -19,13 +19,12 @@
 
 package com.sk89q.worldguard.protection.util;
 
-import com.sk89q.worldedit.regions.CuboidRegion;
-import com.sk89q.worldedit.regions.Polygonal2DRegion;
-import com.sk89q.worldedit.regions.Region;
-import com.sk89q.worldedit.regions.RegionSelector;
+import com.sk89q.worldedit.regions.*;
 import com.sk89q.worldedit.regions.selector.CuboidRegionSelector;
+import com.sk89q.worldedit.regions.selector.CylinderRegionSelector;
 import com.sk89q.worldedit.regions.selector.Polygonal2DRegionSelector;
 import com.sk89q.worldguard.protection.regions.ProtectedCuboidRegion;
+import com.sk89q.worldguard.protection.regions.ProtectedCylinderRegion;
 import com.sk89q.worldguard.protection.regions.ProtectedPolygonalRegion;
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 
@@ -69,6 +68,11 @@ public final class WorldEditRegionConverter {
         if (region instanceof ProtectedPolygonalRegion) {
             return new Polygonal2DRegionSelector(null, region.getPoints(),
                     region.getMinimumPoint().y(), region.getMaximumPoint().y());
+        }
+        if (region instanceof ProtectedCylinderRegion cylinderRegion) {
+            return new CylinderRegionSelector(null,
+                    cylinderRegion.getCenter().toBlockPoint(), cylinderRegion.getRadius(),
+                    cylinderRegion.getMinimumPoint().y(), cylinderRegion.getMaximumPoint().y());
         }
         return null;
     }
